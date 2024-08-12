acadd1e3fe41cdcbdcce78864febf5cb4dd41ca2
Raj Ghosh <89258247+Rajghosh786@users.noreply.github.com>
2024-08-12 21:42:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -98,21 +98,7 @@ searchbtn.addEventListener("click",input=()=>{
                 summary.innerText = data.current.condition.text;
                 kilometer.innerText = wind_kph;
 
-                let miles = kilometer.textContent*0.62;
-                let farenheit = temp.textContent*2+30;
-                change.addEventListener("click",function(){ 
-                    if(temp1.textContent === "°C" && kilometer1.textContent === "kph wind"){
-                        temp1.textContent = "°F"
-                        temp.textContent = farenheit
-                        kilometer1.textContent = "mph wind"
-                        kilometer.textContent = miles
-                    }else{
-                        temp1.textContent = "°C"
-                        temp.innerText = data.current.temp_c;
-                        kilometer1.textContent = "kph wind"
-                        kilometer.textContent = data.current.wind_kph
-                    }
-                })
+
                 function imageChange(){
                     if(summary.innerText === "Cloudy"){
                         cloudyicon.src="cloud.png"
