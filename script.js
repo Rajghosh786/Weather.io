5e2652915b0a8f016b6d347bbe9c52cfff618ec9
Raj Ghosh <89258247+Rajghosh786@users.noreply.github.com>
2024-08-12 21:20:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let isShow = true;
 const test1=document.querySelector('.test1')
 const test2=document.querySelector('.test2')
 const wind1=document.querySelector('.wind1')
@@ -11,14 +10,12 @@ const geo2 = document.querySelector('.geo2')
 const city = document.querySelector('.city')
 const summary = document.querySelector('.summary')
 const kilometer = document.querySelector('.kilometer')
+const kilometer1 = document.querySelector('.kilometer1')
 const image = document.querySelector('.image')
 const cloudyicon = document.querySelector(".cloudyicon")
 const temp1 = document.querySelector('.temp1')
 const button = document.querySelector('.btn')
-const farenheit = document.querySelector('.farenheit')
-const miles = document.querySelector('.miles')
 const change = document.querySelector('.change')
-
 const searchbar = document.querySelector('.searchbar')
 const p = document.querySelector('.p')
 const searchbtn = document.querySelector('.searchbtn')
@@ -40,9 +37,8 @@ const searchbtn = document.querySelector('.searchbtn')
             fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${latitude},${longitude}`, options)
             .then(response => response.json())
             .then(data => {               
-                const {temp_c,temp_f,wind_kph,wind_mph} = data.current
+                const {temp_c,wind_kph} = data.current
                 const {localtime,name} = data.location
-                farenheit.innerText = temp_f;
                 temp.innerText = temp_c;
                 humidity.innerText = data.current.humidity;
                 time.innerText = localtime;        
@@ -51,14 +47,34 @@ const searchbtn = document.querySelector('.searchbtn')
                 city.innerText = name;
                 summary.innerText = data.current.condition.text;
                 kilometer.innerText = wind_kph;
-                miles.innerText = wind_mph;
+                let miles = kilometer.textContent*0.62;
+                let farenheit = temp.textContent*2+30;
+                change.addEventListener("click",function(){ 
+                    if(temp1.textContent === "°C" && kilometer1.textContent === "kph wind"){
+                        temp1.textContent = "°F"
+                        temp.textContent = farenheit
+                        kilometer1.textContent = "mph wind"
+                        kilometer.textContent = miles
+                    }else{
+                        temp1.textContent = "°C"
+                        temp.innerText = data.current.temp_c;
+                        kilometer1.textContent = "kph wind"
+                        kilometer.textContent = data.current.wind_kph
+                    }
+                })
             })
         })    
     }
 })
 
+
+searchbar.addEventListener("keyup",keyboardclick=(keyboard)=>{
+    if(keyboard.which === 13){
+        input();
+    }
+})
 searchbtn.addEventListener("click",input=()=>{
-    p.innerHTML = searchbar.value;
+    p.textContent = searchbar.value;
 
     const options = {
         	method: 'GET',
@@ -67,13 +83,12 @@ searchbtn.addEventListener("click",input=()=>{
         		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
         	}  
         }
-            fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${p.innerHTML}`, options)
+            fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${p.textContent}`, options)
             .then(response => response.json())
             .then((data) => {
-                // console.log(data.current.condition);
-                const {temp_c,temp_f,wind_kph,wind_mph} = data.current
+                console.log(data);
+                const {temp_c,wind_kph} = data.current
                 const {localtime,name} = data.location
-                farenheit.innerText = temp_f;
                 temp.innerText = temp_c;
                 humidity.innerText = data.current.humidity;
                 time.innerText = localtime;        
@@ -82,7 +97,22 @@ searchbtn.addEventListener("click",input=()=>{
                 city.innerText = name;
                 summary.innerText = data.current.condition.text;
                 kilometer.innerText = wind_kph;
-                miles.innerText = wind_mph;
+
+                let miles = kilometer.textContent*0.62;
+                let farenheit = temp.textContent*2+30;
+                change.addEventListener("click",function(){ 
+                    if(temp1.textContent === "°C" && kilometer1.textContent === "kph wind"){
+                        temp1.textContent = "°F"
+                        temp.textContent = farenheit
+                        kilometer1.textContent = "mph wind"
+                        kilometer.textContent = miles
+                    }else{
+                        temp1.textContent = "°C"
+                        temp.innerText = data.current.temp_c;
+                        kilometer1.textContent = "kph wind"
+                        kilometer.textContent = data.current.wind_kph
+                    }
+                })
                 function imageChange(){
                     if(summary.innerText === "Cloudy"){
                         cloudyicon.src="cloud.png"
@@ -96,24 +126,5 @@ searchbtn.addEventListener("click",input=()=>{
                 }
                 imageChange();
                 })
-
             .catch(err => console.error(err));
         });
-        change.addEventListener("click",function(){ 
-            if(isShow){                 
-                test1.style.display = "block";
-                test2.style.display = "none";
-                wind1.style.display = "block";
-                wind2.style.display = "none";
-                isShow = false
-            }else{
-                test1.style.display = "none"
-                test2.style.display = "block";
-                wind1.style.display = "none";
-                wind2.style.display = "block";
-                isShow = true
-                }
-            })
-//enter button not working was not working for me
-//function multiplication toggle was not working for me
-
